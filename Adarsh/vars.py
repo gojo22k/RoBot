2fb374473ca07d828101f806c35cd4fdaaa24d58
Ankit <155639266+gojo22k@users.noreply.github.com>
2024-09-01 14:15:47 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -11,7 +11,7 @@ class Var(object):
     MULTI_CLIENT = False
     API_ID = int(getenv('API_ID', '25198711'))
     API_HASH = str(getenv('API_HASH', '2a99a1375e26295626c04b4606f72752'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN', ''))
+    BOT_TOKEN = str(getenv('BOT_TOKEN', '7411888073:AAHCOUiOncHOfbnzPgrGf699HhhS4gqybX4'))
     name = str(getenv('name', 'robot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
@@ -19,10 +19,10 @@ class Var(object):
     PORT = int(getenv('PORT', 8080))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "1002151954601").split())  
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "1740287480").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
-    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'ANIFLIX'))
+    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'ANIFLIXANKIT'))
     if 'DYNO' in environ:
         ON_HEROKU = True
         APP_NAME = str(getenv('APP_NAME'))
@@ -34,7 +34,7 @@ class Var(object):
     if HAS_SSL:
         URL = "https://{}/".format(FQDN)
     else:
-        URL = "http://{}/".format(FQDN)
+        URL = "https://{}/".format(FQDN)
     DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://Aniflix:Lipun123@aniflix.q2wina5.mongodb.net/?retryWrites=true&w=majority&appName=Aniflix'))
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'Aniflix_Official'))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
